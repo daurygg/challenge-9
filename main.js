104220ea093f6b2f891fe35f1ac9ef1bb651aeb6
devgg03 <134965116+devgg03@users.noreply.github.com>
2024-11-05 15:05:24 -0400

    changes

--- main.js
+++ main.js
@@ -3,27 +3,44 @@
 const form = document.getElementById('form');
 const errorSpan = document.getElementById('error-span');
 const errorform = document.getElementById("email");
+const messages = document.getElementById("messages");
+const singup = document.getElementById("singup");
+const messageBtn = document.getElementById("messageBtn");
+
 
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
 
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
-    console.log(data);
-
-if(data.email || !validarCorreo(data.email)){    
     
-    errorSpan.classList.add("visible-error");
+
+if(!data.email || !validarCorreo(data.email)){   
+
+    errorSpan.classList.remove("visible-error");
     errorform.classList.add("isError");
 }
 else {
-    errorSpan.classList.remove("visible-error");
+    errorSpan.classList.add("visible-error");
     errorform.classList.remove("isError");
+    messages.classList.remove("visible");
+    singup.classList.add("visible");
+
+    const mail = document.getElementById("mail");
+    mail.textContent = data.email;
+    
+    e.target.reset();    
 }
 
-
 });
-function validarCorreo(email) {
+
+messageBtn.addEventListener("click", (e)=>{
+
+    messages.classList.add("visible");
+    singup.classList.remove("visible");      
+});
+
+const validarCorreo = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   }
